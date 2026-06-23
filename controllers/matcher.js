16b62c8a998df10f7854baec20a5bfa583bf759f
Krister Nilsson <krille.home>
2026-06-23 17:57:41 +0200

    260623_1757 - uppdateringstid

--- controllers/matcher.js
+++ controllers/matcher.js
@@ -9,38 +9,49 @@ const toJSON = (obj) =>
     )
   )
 
-  export const getMatcher = async (req ,res) => {
-    const matchSchedule = await prisma.matchen.findMany();
-    res.json(matchSchedule.map(toMatchDto));
-    console.log("Matcherna: ",matchSchedule.map(toMatchDto));
-  }
+export const getMatcher = async (req ,res) => {
+  const matchSchedule = await prisma.matchen.findMany();
+  res.json(matchSchedule.map(toMatchDto));
+  console.log("Matcherna: ",matchSchedule.map(toMatchDto));
+}
 
-  export const getDagensMatcher = async (req ,res) => {
-    const today = new Date().toLocaleDateString('sv-SE')
-    const tomorrowDate = new Date()
-    tomorrowDate.setDate(tomorrowDate.getDate() + 1)
-    const tomorrow = tomorrowDate.toLocaleDateString('sv-SE')
-    
-    console.log(today)     // 2026-06-18
-    console.log(tomorrow)  // 2026-06-19
+export const getDagensMatcher = async (req ,res) => {
+  const today = new Date().toLocaleDateString('sv-SE')
+  const tomorrowDate = new Date()
+  tomorrowDate.setDate(tomorrowDate.getDate() + 1)
+  const tomorrow = tomorrowDate.toLocaleDateString('sv-SE')
+  
+  console.log(today)     // 2026-06-18
+  console.log(tomorrow)  // 2026-06-19
 
-    const matches = await prisma.matchen.findMany({
-      where: {
-        OR: [
-          {
-            datum: today,
-            tid: { gte: '17:45' }
-          },
-          {
-            datum: tomorrow,
-            tid: { lte: '06:15' }
-          }
-        ]
-      }
-    })
-    res.json(matches.map(toMatchDto));
-    console.log("Dagens matcher: ",matches.map(toMatchDto));
-  }
+  const matches = await prisma.matchen.findMany({
+    where: {
+      OR: [
+        {
+          datum: today,
+          tid: { gte: '17:45' }
+        },
+        {
+          datum: tomorrow,
+          tid: { lte: '06:15' }
+        }
+      ]
+    }
+  })
+  res.json(matches.map(toMatchDto));
+  console.log("Dagens matcher: ",matches.map(toMatchDto));
+}
+
+export const getLastUpdate = async (req, res) => {
+  const lastUpdate = await prisma.matchen.aggregate({
+    _max: {
+      updated_at: true
+    }
+  })
+  
+  console.log("Updated at: ",lastUpdate._max.updated_at)
+  res.json(lastUpdate._max.updated_at)
+}
     
 export const setResults = async(req,res) => {
   const results = req.body
--- routes/matcher.js
+++ routes/matcher.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router()
-import {getMatcher, setResults, getDagensMatcher} from "../controllers/matcher.js"
+import {getMatcher, setResults, getDagensMatcher, getLastUpdate} from "../controllers/matcher.js"
 router.get("/", getMatcher)
 router.get("/today", getDagensMatcher)
+router.get("/update", getLastUpdate)
 router.post("/", setResults)
 export default router;
